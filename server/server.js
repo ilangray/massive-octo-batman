ad6364417af8b34c8481b53d140cfca7af978d0c
Michael James <michael@michaelbjames.com>
2013-12-07 15:23:53 -0500

    forgot to take out defunct search route

--- server/server.js
+++ server/server.js
@@ -19,7 +19,6 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/test', routes.index);
-app.get('/search', routes.search);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
