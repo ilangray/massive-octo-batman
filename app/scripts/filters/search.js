fe7f489313a4659aa4d5c1e8ec3ccbde1ba2760a
Michael James <michael@michaelbjames.com>
2013-12-10 11:57:12 -0500

    merged include

--- app/scripts/filters/search.js
+++ app/scripts/filters/search.js
@@ -1,5 +1,4 @@
 angular.module('uselessApp')
-
   .filter('search', function() {
     return function (items, input) {
       // note for MJ: search through given items for classes matching the given input
@@ -8,14 +7,6 @@ angular.module('uselessApp')
         this is just a stub search implementation so we can see that it actually works
         should be replaced with MJ's badass search function(s) 
       */
-      var terms = input.split(" ");
-
-      return _.filter(items, function(course) {
-        return _.some(course, function(value, key) {
-          return _.any(terms, function (t) { 
-            return value.indexOf(t) != -1; 
-          })
-        });
-      });
+      return items;
     }
   });
