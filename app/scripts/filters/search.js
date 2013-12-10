d9feceb9bc219d8834962b722fd3529b0abd724c
Michael James <michael@michaelbjames.com>
2013-12-10 12:05:09 -0500

    load once there's some input

--- app/scripts/filters/search.js
+++ app/scripts/filters/search.js
@@ -7,6 +7,8 @@ angular.module('uselessApp')
         this is just a stub search implementation so we can see that it actually works
         should be replaced with MJ's badass search function(s) 
       */
+      if(_.isUndefined(input))
+        return {};
       return items;
     }
   });
