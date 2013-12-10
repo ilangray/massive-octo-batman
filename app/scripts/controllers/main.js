72ce197a08bf85000c66db1c01506f3cd0030990
Michael James <michael@michaelbjames.com>
2013-12-09 21:38:45 -0500

    replaced data.json

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -123,15 +123,7 @@ angular.module('uselessApp')
         this is just a stub search implementation so we can see that it actually works
         should be replaced with MJ's badass search function(s) 
       */
-      var terms = input.split(" ");
-
-      return _.filter(items, function(course) {
-        return _.some(course, function(value, key) {
-          return _.any(terms, function (t) { 
-            return value.indexOf(t) != -1; 
-          })
-        });
-      });
+      return items;
     }
   })
 
