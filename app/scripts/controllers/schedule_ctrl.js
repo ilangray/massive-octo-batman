13bd63359058186f01b6acff0ac46535cc4f48af
Brendan Conron <conronb@gmail.com>
2013-12-10 21:01:28 -0500

    things happened

--- app/scripts/controllers/schedule_ctrl.js
+++ app/scripts/controllers/schedule_ctrl.js
@@ -1,11 +1,11 @@
 
 angular.module('uselessApp')
   .controller('SchedCtrl', function ($scope, schedule) {
-  	var classes = [];
+  	$scope.classes = [];
     $scope.days = ["Time", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
     $scope.hours = ["7AM", "8AM", "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM", "6PM", "7PM", "8PM", "9PM"];
-  	$scope.$watch(function(){return schedule.classData;}, function(data){
-  		classes = data;
-  		
+  	$scope.$watch(function(){return schedule.classData;}, function(newVal, oldVal){
+  		var course = newVal[newVal.length-1];
+  		$scope.classes.push(course);
   	}, true);
   });
--- app/scripts/services/schedule.js
+++ app/scripts/services/schedule.js
@@ -3,17 +3,11 @@ angular.module('uselessApp')
 .service('schedule', function() {
     var classData = [];
     function addClass(course){
-
-      if(!hasBeenAdded(course)) classData.push(course);
-      else console.log("error");
-      console.log(classData);
-    }
-    function hasBeenAdded(course){
-      for(var index in classData){
-        var identifier = classData[index].$$hashKey;
-        if(classData.$$hashKey === identifier) return true;
+      if(!_.contains(classData, course)){
+        console.log("adding class " + course.name);
+        classData.push(course);
       }
-      return false;
+      else console.log("error");
     }
     return {
       addClass:addClass,
